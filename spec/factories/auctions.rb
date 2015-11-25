7a0d2f43a60db147a9a8fcd7248c5646fceea34f
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-25 07:45:17 -0500

    Generate model auction

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :auction do
+    starts_at "2015-11-25 07:44:27"
+ends_at "2015-11-25 07:44:27"
+time_zone_id "MyString"
+physical_address "MyText"
+name "MyString"
+donation_window_ends_at "2015-11-25 07:44:27"
+  end
+
+end
