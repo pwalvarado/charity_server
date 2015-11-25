d89138847ee4540c3177dc21212074253e5cc1a8
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-25 13:52:36 -0500

    DonationCategory model

---
+++ spec/models/donation_category_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe DonationCategory, type: :model do
+  describe "validations" do
+    it {is_expected.to have_attribute :name}
+
+    it { is_expected.to validate_uniqueness_of :name}
+
+    it { is_expected.to validate_presence_of :name}
+  end
+end
