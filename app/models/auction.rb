e80a2daaf3084fd1230c00ab6bcf81b9300a7600
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-25 13:10:58 -0500

    Auction model, with validations

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,24 @@
 class Auction < ActiveRecord::Base
+  validate :_ends_at_after_starts_at
+  validate :_starts_at_after_donation_window_ends_at
+  validates :time_zone_id, inclusion: { in: ActiveSupport::TimeZone::MAPPING.values,
+    message: "must be in list" }, allow_blank: :true
+
+  def _ends_at_after_starts_at
+    return unless starts_at && ends_at
+
+    if ends_at <= starts_at
+      errors.add :ends_at, "must be greater than starts_at"
+    end
+  end
+
+  def _starts_at_after_donation_window_ends_at
+    return unless starts_at && donation_window_ends_at
+
+    if starts_at <= donation_window_ends_at
+      errors.add :starts_at, "must be greater than donation_window_ends_at"
+    end
+  end
+
+
 end
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,39 @@
-require 'rails_helper'
+RSpec.describe Auction do
+  describe "Attributes" do
+    it { is_expected.to have_attribute :starts_at }
+    it { is_expected.to have_attribute :ends_at }
+    it { is_expected.to have_attribute :time_zone_id }
+    it { is_expected.to have_attribute :physical_address }
+    it { is_expected.to have_attribute :name }
+    it { is_expected.to have_attribute :donation_window_ends_at }
+  end
 
-RSpec.describe Auction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "ends_at is after starts_at" do
+      subject.ends_at = DateTime.new(2015, 11, 24, 0, 0)
+      subject.starts_at = subject.ends_at.advance(seconds: 1)
+      subject.valid?
+      expect(subject.errors[:ends_at]).to include "must be greater than starts_at"
+    end
+
+
+    it "starts_at is after donation_window_ends_at" do
+      subject.starts_at = DateTime.new(2015, 11, 24, 0, 0)
+      subject.donation_window_ends_at = subject.starts_at.advance(seconds: 1)
+      subject.valid?
+      expect(subject.errors[:starts_at]).to include "must be greater than donation_window_ends_at"
+    end
+
+    it "time_zone_id has a valid name" do
+      subject.time_zone_id = "JupiterTime"
+      subject.valid?
+      expect(subject.errors[:time_zone_id]).to include "must be in list"
+    end
+
+    it "time_zone_id can be blank" do
+      subject.time_zone_id = nil
+      subject.valid?
+      expect(subject.errors[:time_zone_id]).to be_empty
+    end
+  end
 end
