df55429335134dff241c8944ae497d9401cdff55
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-25 16:18:46 -0500

    User validations, interesting: validate uniqueness

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
     name "MyString"
-mobile_phone_number "MyString"
-email_address "MyString"
-physical_address "MyString"
+    mobile_phone_number "MyString"
+    email_address "MyString"
+    physical_address "MyString"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 RSpec.describe User do
   describe "Validations" do
+    %w(name mobile_phone_number email_address physical_address).each do |attr|
+      it { is_expected.to have_attribute attr }
+      it { is_expected.to validate_presence_of attr }
+    end
 
+    %w(mobile_phone_number email_address).each do |attr|
+      # it { is_expected.to validate_uniqueness_of attr }
+      # Before doesn't work, Explanation:
+      # http://stackoverflow.com/questions/27046691/cant-get-uniqueness-validation-test-pass-with-shoulda-matcher
+      it do
+        FactoryGirl.create(:user)
+        is_expected.to validate_uniqueness_of attr
+      end
+    end
   end
 end
