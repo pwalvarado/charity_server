750403c9a9b32cacdc9420015576c3261e90822a
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-29 11:43:35 -0500

    Bid validations

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,10 @@
 class Bid < ActiveRecord::Base
   belongs_to :donation
-  belongs_to :bidder
+  belongs_to :bidder, class_name: "User"
+
+  validates :bidder, presence: true
+  validates :donation, presence: true
+  validates :amount_dolars, presence: true
+  validates :quantity, presence: true
+  validates :placed_at, presence: true
 end
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,11 +1,19 @@
 RSpec.describe Bid do
   describe "Attributes" do
-
+    %w(amount_dolars quantity placed_at bidder_id donation_id).each do |attr|
+      it { is_expected.to have_attribute attr }
+    end
   end
 
   describe "Relationships" do
+    %w(donation bidder).each do |attr|
+      it { is_expected.to belong_to attr }
+    end
   end
 
   describe "Validations" do
+    %w(amount_dolars quantity placed_at donation bidder).each do |attr|
+      it { is_expected.to validate_presence_of attr }
+    end
   end
 end
