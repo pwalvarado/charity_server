7dcb8cfbd3b384c22ee4a9e8a223eccc58060a3c
Pedro Alvarado <pedro.alvarado@globant.com>
2015-12-01 12:12:28 -0500

    Spec for authentication: password_digest persistance

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name "MyString"
+    password "1234459000"
     mobile_phone_number "MyString"
     email_address "MyString"
     physical_address "MyString"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
 RSpec.describe User do
+  describe "Attributes" do
+    %w(name mobile_phone_number email_address physical_address).each do |attr|
+      it { is_expected.to have_attribute attr }
+    end
+  end
+
   describe "Relationships" do
     it {is_expected.to have_many :auction_admins }
 
     it {is_expected.to have_many :memberships }
+
+    it { is_expected.to have_many :donations }
   end
 
   describe "Validations" do
     %w(name mobile_phone_number email_address physical_address).each do |attr|
-      it { is_expected.to have_attribute attr }
       it { is_expected.to validate_presence_of attr }
     end
 
@@ -20,9 +27,11 @@ RSpec.describe User do
         is_expected.to validate_uniqueness_of attr
       end
     end
-  end
 
-  describe "Relationships" do
-    it { is_expected.to have_many :donations }
+    it "persists a password_digest based on the password that can be used for authentication" do
+      password = "password"
+      subject = FactoryGirl.create(:user, password: password)
+      expect(subject.authenticate(password)).to eq subject
+    end
   end
 end
