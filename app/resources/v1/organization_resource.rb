79a586ff0e482d4d04038e2a3accffc51a3bfce9
Pedro Alvarado <pedro.alvarado@globant.com>
2015-12-18 14:02:40 -0500

    Add has many memberships relation on organization_resource

--- app/resources/v1/organization_resource.rb
+++ app/resources/v1/organization_resource.rb
@@ -1,5 +1,13 @@
 module V1
   class OrganizationResource < BaseResource
     attribute :name
+    has_many :memberships
+
+    class << self
+      def creatable_fields(context)
+        super - [:memberships]
+      end
+      alias_method :updatable_fields, :creatable_fields
+    end
   end
 end
--- spec/resources/v1/organization_resource_spec.rb
+++ spec/resources/v1/organization_resource_spec.rb
@@ -17,7 +17,7 @@ module V1
     end
 
     it "has the expected fetchable attributes" do
-      expect(subject.fetchable_fields.sort).to eq (creatable_fields + [:id, :created_at, :updated_at]).sort
+      expect(subject.fetchable_fields.sort).to eq (creatable_fields + [:id, :created_at, :updated_at, :memberships]).sort
     end
   end
 end
