b7565b45a01b2c98fda1fbd900294ca6c356149e
Pedro Alvarado <pedro.alvarado@globant.com>
2016-08-05 12:59:22 -0500

    Temporal commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < JSONAPI::ResourceController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def current_user
+    @current_user ||= _doorkeeper_user
+  end
+
+  def _doorkeeper_user
+    return unless valid_doorkeeper_token?
+
+    User.find(doorkeeper_token.resource_owner_id)
+  end
 end
